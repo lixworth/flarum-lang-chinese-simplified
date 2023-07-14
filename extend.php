b3a7551e1b1761940e50f054388cf6e877d952ad
lixworth <lixworth@gmail.com>
2023-07-15 02:15:45 +0800

    chore: wip

--- extend.php
+++ extend.php
@@ -10,10 +10,6 @@
 
 use Flarum\Extend;
 
-
 return [
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/less/admin.less'),
     new Flarum\Extend\LanguagePack
 ];
